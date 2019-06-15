4ae9296a368798a32863a2c420d99cc0df25e7a9
Nicola <niko97.m@gmail.com>
2019-06-15 20:01:52 +0200

    Add War and Battle models

--- ClanWarsManager/manager/admin.py
+++ ClanWarsManager/manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User,Clan
+from .models import User, Clan, War, Battle
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Clan)
+admin.site.register(War)
+admin.site.register(Battle)
--- ClanWarsManager/manager/models.py
+++ ClanWarsManager/manager/models.py
@@ -5,11 +5,22 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class User(models.Model):
     username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=30)
-    clan = models.ForeignKey("Clan",on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    clan = models.ForeignKey("Clan", on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
 class Clan(models.Model):
     name = models.CharField(max_length=30)
     clanMaster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
-    maxPlayers = models.PositiveSmallIntegerField(default=20,validators=[MaxValueValidator(50)])
+    maxPlayers = models.PositiveSmallIntegerField(default=20, validators=[MaxValueValidator(50)])
 
+class War(models.Model):
+    clan = models.ForeignKey(Clan, on_delete=models.CASCADE, related_name="+")
+    date = models.DateField()
+
+class Battle(models.Model):
+    ally = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="+")
+    enemy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    allyDestruction = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], default=0)
+    enemyDestruction = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], default=0)
+    allyVictory = models.BooleanField()
+    war = models.ForeignKey(War, on_delete=models.CASCADE, related_name="+")
 
