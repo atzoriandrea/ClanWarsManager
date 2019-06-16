2193dc47ec361877de65a57fa705260a3493ee22
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2019-06-16 23:30:51 +0200

    Add signup and login views

--- ClanWarsManager/ClanWarsManager/settings.py
+++ ClanWarsManager/ClanWarsManager/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ClanWarsManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'ClanWarsManager.wsgi.application'
 
